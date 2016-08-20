22f88c597bf2f8cf40d20b524aef8db850e5b5ce
Jake Garrison <omonoid@uw.edu>
2016-08-20 14:13:08 -0700

    added dist and data to repo

--- prodServer.js
+++ prodServer.js
@@ -1,7 +1,22 @@
+var path = require('path');
+var express = require('express');
+
+var app = express();
+var port = 5000;
+var staticPath = path.join(__dirname, '/dist');
+app.use(express.static(staticPath));
+
+app.listen(port, function() {
+  console.log('Started production server\n Serving',staticPath,'on port',port);
+  console.log('listening...');
+});
+
 var express = require('express');
 var app     = express();
 
 app.set('port', (process.env.PORT || port));
+var staticPath = path.join(__dirname, '/dist');
+app.use(express.static(staticPath));
 
 //For avoidong Heroku $PORT error
 app.get('/dist', function(request, response) {
