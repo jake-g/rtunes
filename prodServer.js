4a634a55ed3f40d483285fa488a575c633f45bee
Jake Garrison <omonoid@uw.edu>
2016-08-20 14:14:49 -0700

    added dist and data to repo

--- prodServer.js
+++ prodServer.js
@@ -5,15 +5,6 @@ var app = express();
 var port = 5000;
 var staticPath = path.join(__dirname, '/dist');
 app.use(express.static(staticPath));
-
-app.listen(port, function() {
-  console.log('Started production server\n Serving',staticPath,'on port',port);
-  console.log('listening...');
-});
-
-var express = require('express');
-var app     = express();
-
 app.set('port', (process.env.PORT || port));
 var staticPath = path.join(__dirname, '/dist');
 app.use(express.static(staticPath));
