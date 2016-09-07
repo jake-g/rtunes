dde03445c7fc30045c811f34ba05fc7c3f43c72c
Jake Garrison <omonoid@uw.edu>
2016-09-06 22:44:40 -0700

    About page

--- src/containers/About.js
+++ src/containers/About.js
@@ -8,6 +8,8 @@ const FETCH_REDDIT_URL = 'https://github.com/CookPete/fetch-reddit'
 const REACT_PLAYER_URL = 'https://github.com/CookPete/react-player'
 const CURATION_URL = 'https://www.reddit.com/r/listentothis/comments/1iwc8n/meta_announcing_the_official_rlistentothis_music/'
 const LISTENTOTHIS_URL = 'https://www.reddit.com/r/listentothis'
+const CREATOR_URL = 'https://github.com/CookPete'
+const RPLAYR_URL = 'https://github.com/CookPete/rplayr'
 
 export default function About() {
 
@@ -20,19 +22,28 @@ export default function About() {
       <dl style={{marginLeft: '20px'}}>
         <dt>What is this?</dt>
         <dd>
-          {APP_NAME} creates playlists from any Reddit comment thread, subreddit or multi-reddit. Just replace <strong>reddit.com</strong> in any Reddit URL to <strong>{window.location.host}</strong> and you’re good.
+           {APP_NAME} creates playlists from content posted on reddit. These playlists are crowdsourced rather than created from an algorithm like Spotify or Pandora.
+        </dd>
+        <dt>What types of stations are there?</dt>
+        <dd>
+          <ul>
+            <li><b>playlists :</b> tunes collected from 100's of curated subreddits, which can be thought of as more specific collections than genres</li>
+            <li><b>genres :</b> collections of similar playlists combined to deliver a broader collection of tunes that fit a genre</li>
+            <li><b>discover :</b> tunes pulled from all music related subreddits based off activity and moderation level</li>
+            <li><b>threads :</b> tunes pulled from comments that respond to a osted question or concept</li>
+          </ul>
+        </dd>
+        <dt>How can I sort the tunes?</dt>
+        <dd>
+          <ul>
+            <li><b>hot :</b> based off activity, and age (default).</li>
+            <li><b>new :</b> based purely from time posted. newest on top</li>
+            <li><b>top :</b> based off upvotes in a selected time window</li>
+          </ul>
         </dd>
         <dt>How does it work?</dt>
         <dd>
-          The app is written using <a href={REACT_URL} target='_blank'>React</a>. It fetches stuff from Reddit using <a href={FETCH_REDDIT_URL} target='_blank'>fetch-reddit</a>, then <a href={REACT_PLAYER_URL} target='_blank'>react-player</a> plays any supported URLs. See the <a href={SOURCE_URL} target='_blank'>source code</a> on GitHub. Playlists are curated by the mighty <a href={CURATION_URL} target='_blank'>u/evilnight</a> over at <a href={LISTENTOTHIS_URL} target='_blank'>r/listentothis</a>.
-        </dd>
-        <dt>What can it play?</dt>
-        <dd>
-          {APP_NAME} currently supports YouTube, SoundCloud and Vimeo links.
-        </dd>
-        <dt>What about <a href='http://radd.it' target='_blank'>radd.it</a>, <a href='https://reddit.musicplayer.io' target='_blank'>reddit.musicplayer.io</a>, <a href='http://redditplaylister.phoenixforgotten.com' target='_blank'>redditplaylister</a>, etc?</dt>
-        <dd>
-          Yes, there are other apps that do the job. I wanted to create something simple with a clean, minimal interface. Also, this app fixes <a href='https://github.com/CookPete/react-player/issues/7' target='_blank'>issues</a> that other apps still suffer from.
+          {APP_NAME} is heavily based of <a href={CREATOR_URL} target='_blank'>cookpete's</a> work: <a href={RPLAYR_URL} target='_blank'>rplayer.com</a>. {APP_NAME} is written using <a href={REACT_URL} target='_blank'>React</a>. I modified rplayr to be more compatible with my listenting habits and have more features (more to come too!). it fetches content from Reddit using <a href={FETCH_REDDIT_URL} target='_blank'>fetch-reddit</a>, then <a href={REACT_PLAYER_URL} target='_blank'>react-player</a> plays any supported URLs. Thanks again to <a href={CREATOR_URL} target='_blank'>CookPete</a> for creating these awesome open-source tools. See the <a href={SOURCE_URL} target='_blank'>source code</a> on GitHub. Genres are curated by <a href={CURATION_URL} target='_blank'>u/evilnight</a> over at <a href={LISTENTOTHIS_URL} target='_blank'>r/listentothis</a>.
         </dd>
       </dl>
     </section>
