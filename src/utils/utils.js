bb9db5326b66cde35bf365daf017d9fa3ff35bd3
Jake Garrison <omonoid@uw.edu>
2017-01-18 02:52:07 -0800

    added mob user detect

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -55,3 +55,19 @@ export function dimensions() {  // window dimensions
       app_height: document.getElementById('app').offsetHeight
     });
 }
+
+export function detect_mobile() { 
+ if( navigator.userAgent.match(/Android/i)
+ || navigator.userAgent.match(/webOS/i)
+ || navigator.userAgent.match(/iPhone/i)
+ || navigator.userAgent.match(/iPad/i)
+ || navigator.userAgent.match(/iPod/i)
+ || navigator.userAgent.match(/BlackBerry/i)
+ || navigator.userAgent.match(/Windows Phone/i)
+ ){
+    return true;
+  }
+ else {
+    return false;
+  }
+}
