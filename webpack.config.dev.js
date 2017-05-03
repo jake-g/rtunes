6001a5f8b6c3ac35107931a7f5fa036367e111e1
Jake Garrison <omonoid@uw.edu>
2017-05-03 00:17:29 -0700

    updated webpack to latest - removed postcss-loader to avoid error. hopfully that is ok

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,59 +3,59 @@ const webpack = require('webpack');
 
 
 module.exports = {
-  devtool: 'cheap-module-eval-source-map',
-  entry: [
-    'babel-polyfill',
-    'webpack-hot-middleware/client',
-    './src/index'
-  ],
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'app.js',
-    publicPath: '/'
-  },
-  plugins: [
-    /**
-     * This is where the magic happens! You need this to enable Hot Module Replacement!
-     */
-    new webpack.HotModuleReplacementPlugin(),
-    /**
-     * NoErrorsPlugin prevents your webpack CLI from exiting with an error code if
-     * there are errors during compiling - essentially, assets that include errors
-     * will not be emitted. If you want your webpack to 'fail', you need to check out
-     * the bail option.
-     */
-    new webpack.NoErrorsPlugin(),
-    /**
-     * DefinePlugin allows us to define free variables, in any webpack build, you can
-     * use it to create separate builds with debug logging or adding global constants!
-     * Here, we use it to specify a development build.
-     */
-    // new webpack.DefinePlugin({
-    //   'process.env.NODE_ENV': JSON.stringify('development')
-    // }),
+	devtool: 'cheap-module-eval-source-map',
+	entry: [
+		'babel-polyfill',
+		'webpack-hot-middleware/client',
+		'./src/index'
+	],
+	output: {
+		path: path.join(__dirname, 'dist'),
+		filename: 'app.js',
+		publicPath: '/'
+	},
+	plugins: [
+		/**
+		 * This is where the magic happens! You need this to enable Hot Module Replacement!
+		 */
+		new webpack.HotModuleReplacementPlugin(),
+		/**
+		 * NoEmitOnErrorsPlugin prevents your webpack CLI from exiting with an error code if
+		 * there are errors during compiling - essentially, assets that include errors
+		 * will not be emitted. If you want your webpack to 'fail', you need to check out
+		 * the bail option.
+		 */
+		new webpack.NoEmitOnErrorsPlugin(),
+		/**
+		 * DefinePlugin allows us to define free variables, in any webpack build, you can
+		 * use it to create separate builds with debug logging or adding global constants!
+		 * Here, we use it to specify a development build.
+		 */
+		// new webpack.DefinePlugin({
+		//   'process.env.NODE_ENV': JSON.stringify('development')
+		// }),
 
-  ],
-  module: {
-    loaders: [{
-      test: /\.js$/,
-      loader: 'babel',
-      include: path.join(__dirname, 'src')
-    }, {
-      test: /\.json$/,
-      loader: 'json',
-      include: path.join(__dirname, 'data')
-    }, {
-      test: /\.scss$/,
-      loader: 'style!css?modules&sourceMap&localIdentName=[name]-[local]!sass?sourceMap',
-      include: path.join(__dirname, 'src')
-    }, {
-      test: /\.css$/,
-      loader: 'style?insertAt=top!css',
-      include: [
-        path.join(__dirname, 'src'),
-        path.join(__dirname, 'node_modules', 'normalize.css')
-      ]
-    }]
-  }
+	],
+	module: {
+		loaders: [{
+			test: /\.js$/,
+			loader: 'babel-loader',
+			include: path.join(__dirname, 'src')
+		}, {
+			test: /\.json$/,
+			loader: 'json-loader',
+			include: path.join(__dirname, 'data')
+		}, {
+			test: /\.scss$/,
+			loader: 'style-loader!css-loader?modules&sourceMap&localIdentName=[name]-[local]!sass-loader?sourceMap',
+			include: path.join(__dirname, 'src')
+		}, {
+			test: /\.css$/,
+			loader: 'style-loader?insertAt=top!css-loader',
+			include: [
+				path.join(__dirname, 'src'),
+				path.join(__dirname, 'node_modules', 'normalize.css')
+			]
+		}]
+	}
 }
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -4,64 +4,66 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin')
 var autoprefixer = require('autoprefixer')
 
 module.exports = {
-  devtool: 'source-map',
-  entry: [
-    'babel-polyfill',
-    './src/index'
-  ],
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'app.js',
-    publicPath: '/'
-  },
-  plugins: [
-    /**
-     * This plugin assigns the module and chunk ids by occurence count. What this
-     * means is that frequently used IDs will get lower/shorter IDs - so they become
-     * more predictable.
-     */
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production')
-    }),
-    /**
-     * Some of you might recognize this! It minimizes all your JS output of chunks.
-     * Loaders are switched into a minmizing mode. Obviously, you'd only want to run
-     * your production code through this!
-     */
-    new webpack.optimize.UglifyJsPlugin({
-      compressor: { warnings: false },
+	devtool: 'source-map',
+	entry: [
+		'babel-polyfill',
+		'./src/index'
+	],
+	output: {
+		path: path.join(__dirname, 'dist'),
+		filename: 'app.js',
+		publicPath: '/'
+	},
+	plugins: [
+		/**
+		 * This plugin assigns the module and chunk ids by occurence count. What this
+		 * means is that frequently used IDs will get lower/shorter IDs - so they become
+		 * more predictable.
+		 */
+		new webpack.optimize.OccurrenceOrderPlugin(),
+		new webpack.DefinePlugin({
+			'process.env.NODE_ENV': JSON.stringify('production')
+		}),
+		/**
+		 * Some of you might recognize this! It minimizes all your JS output of chunks.
+		 * Loaders are switched into a minmizing mode. Obviously, you'd only want to run
+		 * your production code through this!
+		 */
+		new webpack.optimize.UglifyJsPlugin({
+			compressor: {
+				warnings: false
+			},
 
-      comments: false
-    }),
+			comments: false
+		}),
     new ExtractTextPlugin('app.css')
-  ],
-  module: {
-    loaders: [{
-      test: /\.js$/,
-      loader: 'babel',
-      include: path.join(__dirname, 'src')
-    }, {
-      test: /\.json$/,
-      loader: 'json',
-      include: path.join(__dirname, 'data')
-    }, {
-      test: /\.scss$/,
-      loader: ExtractTextPlugin.extract(
-        'style',
-        'css?modules&sourceMap&localIdentName=[hash:base64:3]&importLoaders=1!postcss!sass?sourceMap'
-      ),
-      include: path.join(__dirname, 'src')
-    }, {
-      test: /\.css$/,
-      loader: ExtractTextPlugin.extract('style', 'css'),
-      include: [
-        path.join(__dirname, 'src'),
-        path.join(__dirname, 'node_modules', 'normalize.css')
-      ]
-    }]
-  },
-  postcss: function () {
-    return [ autoprefixer ]
-  }
+	],
+	module: {
+		loaders: [{
+			test: /\.js$/,
+			loader: 'babel-loader',
+			include: path.join(__dirname, 'src')
+		}, {
+			test: /\.json$/,
+			loader: 'json-loader',
+			include: path.join(__dirname, 'data')
+		}, {
+			test: /\.scss$/,
+			loader: ExtractTextPlugin.extract({
+				fallback: 'style-loader',
+				use: 'css-loader?modules&sourceMap&localIdentName=[hash:base64:3]&importLoaders=1!sass-loader?sourceMap'
+			}),
+			include: path.join(__dirname, 'src')
+		}, {
+			test: /\.css$/,
+			loader: ExtractTextPlugin.extract({
+				fallback: 'style-loader',
+				use: 'css-loader'
+			}),
+			include: [
+				path.join(__dirname, 'src'),
+				path.join(__dirname, 'node_modules', 'normalize.css')
+			]
+		}]
+	}
 }
